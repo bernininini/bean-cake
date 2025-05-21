d1b08cb8440a4e02efa86979732f33211cac6d76
berni <150194064+bernininini@users.noreply.github.com>
2025-05-21 18:26:54 -0400

    Update bot.ino

--- bot.ino
+++ bot.ino
@@ -1,45 +1,83 @@
+// BEAN-CAKE BOT: ESP32-S3 CAM with motor control and camera stream
+#include "esp_camera.h"
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-const char* ssid = "ESP32-CAM";
-const char* pass = "12345678";
+const char* ssid = "BEANCAKE";
+const char* pwd = "beancake123";
+WiFiUDP udp;
 
-WiFiUDP u;
-int p = 1234;
+#define IN1 12
+#define IN2 13
+#define IN3 14
+#define IN4 15
+#define ENA 2
+#define ENB 4
 
-#define A1 14
-#define A2 15
-#define PWMA 12
-#define B1 13
-#define B2 2
-#define PWMB 4
+int spdL = 0;
+int spdR = 0;
 
-void m(int p1, int d1, int d2, int p) {
-  digitalWrite(d1, p1 > 0);
-  digitalWrite(d2, p1 < 0);
-  analogWrite(p, abs(p1));
+void stM(int l, int r) {
+  analogWrite(ENA, abs(l));
+  digitalWrite(IN1, l > 0);
+  digitalWrite(IN2, l < 0);
+  analogWrite(ENB, abs(r));
+  digitalWrite(IN3, r > 0);
+  digitalWrite(IN4, r < 0);
+}
+
+void hndUDP() {
+  int p = udp.parsePacket();
+  if (p) {
+    char d[8];
+    udp.read(d, 8);
+    spdL = (d[0] << 8) | d[1];
+    spdR = (d[2] << 8) | d[3];
+    stM(spdL, spdR);
+  }
+}
+
+void stWiFi() {
+  WiFi.softAP(ssid, pwd);
+  udp.begin(1234);
+}
+
+void stPWM() {
+  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
+  ledcAttachPin(ENA, 0); ledcSetup(0, 1000, 8);
+  ledcAttachPin(ENB, 1); ledcSetup(1, 1000, 8);
+}
+
+void stCam() {
+  camera_config_t cfg;
+  cfg.ledc_channel = LEDC_CHANNEL_0;
+  cfg.ledc_timer = LEDC_TIMER_0;
+  cfg.pin_d0 = 5; cfg.pin_d1 = 18; cfg.pin_d2 = 19; cfg.pin_d3 = 21;
+  cfg.pin_d4 = 36; cfg.pin_d5 = 39; cfg.pin_d6 = 34; cfg.pin_d7 = 35;
+  cfg.pin_xclk = 0; cfg.pin_pclk = 22; cfg.pin_vsync = 25; cfg.pin_href = 23;
+  cfg.pin_sscb_sda = 26; cfg.pin_sscb_scl = 27; cfg.pin_pwdn = 32;
+  cfg.pin_reset = -1; cfg.pin_xclk = 0;
+  cfg.xclk_freq_hz = 20000000;
+  cfg.pixel_format = PIXFORMAT_JPEG;
+  cfg.frame_size = FRAMESIZE_QVGA;
+  cfg.jpeg_quality = 10;
+  cfg.fb_count = 1;
+  esp_camera_init(&cfg);
+  server.on("/cam", HTTP_GET, [](AsyncWebServerRequest *r){
+    camera_fb_t *f = esp_camera_fb_get();
+    r->send_P(200, "image/jpeg", f->buf, f->len);
+    esp_camera_fb_return(f);
+  });
+  server.begin();
 }
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.softAP(ssid, pass);
-  u.begin(p);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-  pinMode(B1, OUTPUT);
-  pinMode(B2, OUTPUT);
-  pinMode(PWMB, OUTPUT);
+  stPWM();
+  stWiFi();
+  stCam();
 }
 
 void loop() {
-  int pLen = u.parsePacket();
-  if (pLen) {
-    char b[20];
-    u.read(b, sizeof(b));
-    int l = 0, r = 0;
-    sscanf(b, "%d,%d", &l, &r);
-    m(l, A1, A2, PWMA);
-    m(r, B1, B2, PWMB);
-  }
+  hndUDP();
 }
